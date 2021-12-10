fd364db9f0af403d682a593f4ee7dd3d7aa20bb1
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2021-12-10 05:11:06 +0000

    pointers incomplete

---
+++ Module-02 Programs/06-pointers-program.c
@@ -0,0 +1,5 @@
+/* POINTERS
+
+In C Language pointer is a variable representing the location of an item.
+
+Let 'a' be variable.  The compiler will allocate some memory for this variable.  The address of 'a' can be determined by '&' address operator.
